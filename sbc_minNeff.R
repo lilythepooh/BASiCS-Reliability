a6c37cb2db8ebc130f7c83c242bfa82ad32a2924
Sijia Li <47448091+lilythepooh@users.noreply.github.com>
2021-12-21 10:32:08 +0000

    Update sbc_minNeff.R

--- sbc_minNeff.R
+++ sbc_minNeff.R
@@ -29,59 +29,14 @@ sim_Output_nu<-read.table(paste0(data.path,"/sim_nu_",index_run,".txt"))
 sim_Output_phi<-read.table(paste0(data.path,"/sim_phi_",index_run,".txt"))
 sim_Output_s<-read.table(paste0(data.path,"/sim_s_",index_run,".txt"))
 sim_Output_theta<-read.table(paste0(data.path,"/sim_theta_",index_run,".txt"))
-#for (samp in (1:500)){
- 
- # post_delta<-sim_Output_delta[samp,]
-  #post_mu<-sim_Output_mu[samp,]
-  #post_nu<-sim_Output_nu[samp,]
-  #post_phi<-sim_Output_phi[samp,]
-  #post_s<-sim_Output_s[samp,]
-  #post_theta<-sim_Output_theta[samp,]
- # g_samp[samp]<-g1d(post_delta,post_mu,post_nu,post_phi,post_s,post_theta)
-#}
-
-#compute log-m autocorrelation
-#autoCorr<-rep(0,499)
-#for (lag in (0:499)){
-# autoCorr[lag]<-0
-#for (k in (1:(500-lag))){
-# autoCorr[lag]<-autoCorr[lag]+g_samp[k]*g_samp[k+lag]
-#}
-#autoCorr[lag]<-autoCorr[lag]/(500-lag)
-#}
-
-#Compute the number of effective samples
-#N_eff=500/(1+sum(autoCorr))
 Ess<-c(ESS(sim_Output_delta),ESS(sim_Output_mu),ESS(sim_Output_nu),ESS(sim_Output_phi),ESS(sim_Output_s),ESS(sim_Output_theta))
 write.table(Ess,paste0("ESS_",index_run,".txt"))
 N_eff=min(Ess)
-#Define the function for rank statistic calculation
-#rank_statistic<-function(g0,g_samp){
-#  rank<-0
- # for (l in (1:L)){
-  #  if (g_samp[l]<g0){
-   #   rank=rank+1
-    #}
-    #else{
-     # rank=rank
-    #}
-#  }
- # return(rank)
-#}
-
 #Set L=N_samp/10=50, 
 L=50
 #then
 if (N_eff>=L){
-  #Thin the sample to L samples
-  #thin_seq<-seq(1,length(g_samp),10)
-  #g_samp_new<-g_samp[thin_seq]
-  #Compute the rank statistic
-  #r<-rank_statistic(g0,g_samp_new) 
   print(index_run)
-  #write.table(g_samp,paste0("sim_g_samp_",index_run,".txt"))
-  #write.table(g0,paste0("sim_g0_",index_run,".txt"))
-  #write.table(r,paste0("sim_rank_",index_run,".txt"))
 }else {
   N_new=2*round((50*15000/N_eff))+10000
   n_new=10000
@@ -129,28 +84,6 @@ if (N_eff>=L){
   write.table(sim_Output_phi,paste0("sim_phi_",index_run, ".txt"))
   sim_Output_nu<-sim_Output@parameters$nu
   write.table(sim_Output_nu,paste0("sim_nu_",index_run, ".txt"))
-  #Compute g for the prior value
-  #g0<-g1d(delta1,mu1,nu1,phi1,s1,theta1)
-  #Compute g for the posterior samples
-  #g_samp<-rep(0,L)
-  #for (samp in (1:L)){
-   # post_delta<-sim_Output_delta[samp,]
-    #post_mu<-sim_Output_mu[samp,]
-    #post_nu<-sim_Output_nu[samp,]
-    #post_phi<-sim_Output_phi[samp,]
-    #post_s<-sim_Output_s[samp,]
-    #post_theta<-sim_Output_theta[samp,]
-    #g_samp[samp]<-g1d(post_delta,post_mu,post_nu,post_phi,post_s,post_theta)
-  #}
-  #Thin the sample to L samples
-  #thin_seq<-seq(1,length(g_samp),length(g_samp)/L)
-  
-  #g_samp_new<-g_samp[thin_seq]
-  #r<-rank_statistic(g0,g_samp_new)
- # write.table(r,paste0("sim_rank_",index_run,".txt"))
- # write.table(g_samp,paste0("sim_g_samp_",index_run,".txt"))
- # write.table(g0,paste0("sim_g0_",index_run,".txt"))
- # write.table(N_eff,paste0("N_eff_",index_run,".txt"))
 }
 }
 
